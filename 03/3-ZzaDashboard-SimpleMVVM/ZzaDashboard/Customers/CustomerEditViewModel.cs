c22d665bcc90ea460e907ed0b1d17f626989a9d4
oleg-glushko <oleg.glushko@outlook.com>
2023-09-11 11:32:10 +1000

    Remove CommunityToolkit.Mvvm dependency

--- 03/3-ZzaDashboard-SimpleMVVM/ZzaDashboard/Customers/CustomerEditViewModel.cs
+++ 03/3-ZzaDashboard-SimpleMVVM/ZzaDashboard/Customers/CustomerEditViewModel.cs
@@ -1,5 +1,4 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Windows.Input;
 using Zza.Data;
---
+++ 03/3-ZzaDashboard-SimpleMVVM/ZzaDashboard/RelayCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ZzaDashboard;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action _TargetExecuteMethod;
+    private readonly Func<bool>? _TargetCanExecuteMethod;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public RelayCommand(Action executeMethod)
+    {
+        _TargetExecuteMethod = executeMethod;
+    }
+
+    public RelayCommand(Action executeMethod, Func<bool> canExecuteMethod)
+    {
+        _TargetExecuteMethod = executeMethod;
+        _TargetCanExecuteMethod = canExecuteMethod;
+    }
+
+    public void RiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (_TargetCanExecuteMethod != null)
+            return _TargetCanExecuteMethod();
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        _TargetExecuteMethod();
+    }
+}
+
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T> _TargetExecuteMethod;
+    private readonly Func<T?, bool>? _TargetCanExecuteMethod;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public RelayCommand(Action<T> executeMethod)
+    {
+        _TargetExecuteMethod = executeMethod;
+    }
+
+    public RelayCommand(Action<T> executeMethod, Func<T?, bool> canExecuteMethod)
+    {
+        _TargetExecuteMethod = executeMethod;
+        _TargetCanExecuteMethod = canExecuteMethod;
+    }
+
+    public void RiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (_TargetCanExecuteMethod != null)
+        {
+            var param = (T?)parameter;
+            return _TargetCanExecuteMethod(param);
+        }
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        T? param = (T?)parameter;
+        if (param != null)
+            _TargetExecuteMethod(param);
+    }
+}
